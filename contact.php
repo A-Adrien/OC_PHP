4d0df9909e7ee5e125ec9d7c5c16fc7282db9c4d
adrien <astadrien21@gmail.com>
2023-11-15 10:55:49 +0100

    Adding a file input to the form with some security with it

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@
 
     <?php include_once('header.php'); ?>
         <h1>Contactez nous</h1>
-        <form action="submit_contact.php" method="POST">
+        <form action="submit_contact.php" method="POST" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
@@ -25,6 +25,10 @@
             <div class="mb-3">
                 <label for="message" class="form-label">Votre message</label>
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="screenshot" class="form-label">Votre capture d'écran</label>
+                <input type="file" class="form-control" name="screenshot" id="screenshot">
             </div>
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
--- submit_contact.php
+++ submit_contact.php
@@ -5,6 +5,33 @@ if (!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAI
 
     return;
 }
+//verification fichier
+if ($_FILES['screenshot']['error'] != 4 && $_FILES['screenshot']['error'] == 0) {
+
+    if ($_FILES['screenshot']['size'] <= 1000000) {
+
+        $fileInfo = pathinfo($_FILES['screenshot']['name']);
+        $extension = $fileInfo['extension'];
+        $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        if (in_array($extension, $allowedExtensions)) {
+            
+            move_uploaded_file($_FILES['screenshot']['tmp_name'], 'uploads/' .$_FILES['screenshot']['name']);
+            echo 'Le fichier a bien été envoyé !';
+
+        } else {
+            echo 'Le fichier n\'est pas dans une bonne extension (jpg, jpeg, png, gif).';
+            return;
+        }
+
+    } else {
+        echo 'Votre fichier est trop volumineux. Il ne doit pas être supérieur à 1 Mo.';
+        return;
+    }
+
+} else {
+    echo 'Une erreur s\'est produite avec votre fichier (' .$_FILES['screenshot']['error']. ').';
+    return;
+}
 ?>
 
 <h1>Message bien reçu !</h1>
